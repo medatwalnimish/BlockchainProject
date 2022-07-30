457952edfce9179b4f10f2d6e59201d0020ca6f5
nimishmedatwal <medatwalnimishm@gmail.com>
2022-07-31 01:10:59 +0530

    made register

--- Backend/home/users/views.py
+++ Backend/home/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from .serializers import UserSerializers
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer=UserSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
