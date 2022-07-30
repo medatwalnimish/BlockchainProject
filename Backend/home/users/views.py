0240a55718d362a6e02872f154af7a1ee001778e
nimishmedatwal <medatwalnimishm@gmail.com>
2022-07-31 01:27:16 +0530

    added LoginView

--- Backend/home/users/views.py
+++ Backend/home/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response 
+from .models import User
 from .serializers import UserSerializers
 # Create your views here.
 
@@ -10,3 +12,20 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        return Response({
+            'message':'success'
+        })
