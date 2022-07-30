235382065bc4bf2f42be9e9b85900f85261731bb
nimishmedatwal <medatwalnimishm@gmail.com>
2022-07-31 01:31:18 +0530

    added jwt token

--- Backend/home/users/views.py
+++ Backend/home/users/views.py
@@ -4,6 +4,8 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response 
 from .models import User
 from .serializers import UserSerializers
+import jwt, datetime
+
 # Create your views here.
 
 class RegisterView(APIView):
@@ -26,6 +28,18 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
         
-        return Response({
-            'message':'success'
-        })
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
