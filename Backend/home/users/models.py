9ac551fdbd2a4350b5c89e602e15bee0141d3b7c
Bhawna-3001 <bagarwal.thapar@gmail.com>
2022-08-03 23:27:35 +0530

    added refresh tokens

--- Backend/home/users/models.py
+++ Backend/home/users/models.py
@@ -11,3 +11,14 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+class NFT_Details(models.Model):
+    certificate_id = models.CharField(max_length=200,default=0)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE, default=0)
+    token_url = models.CharField(max_length=200,default='')
+    expiry_date = models.DateTimeField(default='')
+    token_id = models.IntegerField(unique=True,default=0)
+    acc_address = models.CharField(max_length=300,default='')
+    redeem = models.BooleanField(default=True)
+    def str(self):
+        return str(self.token_id)
--- Backend/home/users/views.py
+++ Backend/home/users/views.py
@@ -1,41 +1,75 @@
 from django.shortcuts import render
+from rest_framework.authentication import get_authorization_header
+from rest_framework.exceptions import APIException, AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response 
 from .models import User
 from .serializers import UserSerializers
+from .authentication import create_access_token, create_refresh_token, decode_access_token, decode_refresh_token
 import jwt, datetime
 
 # Create your views here.
 
-class RegisterView(APIView):
+class RegisterAPIView(APIView):
     def post(self,request):
         serializer=UserSerializers(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-class LoginView(APIView):
+class LoginAPIView(APIView):
     def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
+        user = User.objects.filter(email=request.data['email']).first()
 
-        user = User.objects.filter(email=email).first()
+        if not user:
+            raise APIException('Invalid credentials!')
 
-        if user is None:
-            raise AuthenticationFailed('User not found!')
+        if not user.check_password(request.data['password']):
+            raise APIException('Invalid credentials!')
 
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password!')
-        
-        payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
+        response = Response()
+
+        response.set_cookie(key='refreshToken', value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
         }
 
-        token = jwt.encode(payload, 'secret', algorithm='HS256')
-        
+        return response
+
+class UserAPIView(APIView):
+    def get(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+
+            user = User.objects.filter(pk=id).first()
+
+            return Response(UserSerializers(user).data)
+
+        raise AuthenticationFailed('unauthenticated')
+
+
+class RefreshAPIView(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refreshToken')
+        id = decode_refresh_token(refresh_token)
+        access_token = create_access_token(id)
         return Response({
-            'jwt':token
+            'token': access_token
         })
+
+
+class LogoutAPIView(APIView):
+    def post(self, _):
+        response = Response()
+        response.delete_cookie(key="refreshToken")
+        response.data = {
+            'message': 'success'
+        }
+        return response
