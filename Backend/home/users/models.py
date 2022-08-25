3a409ac900384692934cfad504e890ca97ea161a
nimishmedatwal <medatwalnimishm@gmail.com>
2022-08-25 16:42:31 +0530

    resolving

--- Backend/home/users/models.py
+++ Backend/home/users/models.py
@@ -15,6 +15,8 @@ class User(AbstractUser):
 class NFT_Details(models.Model):
     certificate_id = models.CharField(max_length=200,default=0)
     user_id = models.ForeignKey(User,on_delete=models.CASCADE, default=0)
+    created_at = models.DateTimeField(default='')
+    updated_at = models.DateTimeField(default=timezone.now)
     token_url = models.CharField(max_length=200,default='')
     expiry_date = models.DateTimeField(default='')
     token_id = models.IntegerField(unique=True,default=0)
@@ -22,13 +24,36 @@ class NFT_Details(models.Model):
     redeem = models.BooleanField(default=True)
     def str(self):
         return str(self.token_id)
-    
+
+
+def document_filepath(self, filename):
+    return 'document/' + str(self.pk) + '/docuemnt.pdf'
+
+
 class document(models.Model):
     user_id = models.ForeignKey(User,on_delete=models.CASCADE, default=0)
     created_at = models.DateTimeField(default='')
-    updated_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=UTC().now)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=200,default='')
-    file_link = models.URLField(max_length = 300)
+    file_link = models.FileField(upload_to=document_filepath,null=True)
+
+    def doucument_filename(self):
+        return str(self.product_image)[str(self.product_image).index('product_images/' + str(self.pk) + "/"):]
     def str(self):
         return str(self.token_id)
+
+
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=60, null=True)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(max_length=255, upload_to=get_product_image_filepath, null=True, blank=True)
+
+    def get_product_image_filename(self):
+        return str(self.product_image)[str(self.product_image).index('product_images/' + str(self.pk) + "/"):]
+
+    def __str__(self):
+        return self.name
